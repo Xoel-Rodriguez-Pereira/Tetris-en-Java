223fb7c2f4b9acf889fcf3c73d5e6e505587ec43
Xoel-Rodriguez-Pereira.com <xoelrodriguezpereira@gmail.com>
2026-07-03 02:10:41 +0200

    feature: hacer controles compatibles con las flechas direccionales del teclado

--- app/src/main/java/org/xoelrp/tetris/game_panel/KeyHandler.java
+++ app/src/main/java/org/xoelrp/tetris/game_panel/KeyHandler.java
@@ -14,16 +14,16 @@ public class KeyHandler implements KeyListener {
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
 
-        if (code == KeyEvent.VK_W) {
+        if (code == KeyEvent.VK_W || code == KeyEvent.VK_UP) {
             upKeyPress = true;
         }
-        if (code == KeyEvent.VK_S) {
+        if (code == KeyEvent.VK_S || code == KeyEvent.VK_DOWN) {
             downKeyPress = true;
         }
-        if (code == KeyEvent.VK_A) {
+        if (code == KeyEvent.VK_A || code == KeyEvent.VK_LEFT) {
             leftKeyPress = true;
         }
-        if (code == KeyEvent.VK_D) {
+        if (code == KeyEvent.VK_D || code == KeyEvent.VK_RIGHT) {
             rightKeyPress = true;
         }
         if (code == KeyEvent.VK_R) {
